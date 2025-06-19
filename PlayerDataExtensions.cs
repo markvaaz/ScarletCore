a1b9a1ef359993ce84b061061d53c838b2fc47c1
Mark Vaaz <marcosrodolfo32@gmail.com>
2025-06-19 18:26:25 -0300

    Add PlayerData extension methods for retrieving player data by various identifiers

---
+++ PlayerDataExtensions.cs
@@ -0,0 +1,35 @@
+using ProjectM;
+using ProjectM.Network;
+using ScarletCore.Data;
+using ScarletCore.Services;
+using Unity.Entities;
+
+namespace ScarletCore;
+
+public static class PlayerDataExtensions {
+  public static PlayerData GetPlayerData(this string playerName) {
+    return PlayerService.TryGetByName(playerName, out PlayerData playerData) ? playerData : null;
+  }
+
+  public static PlayerData GetPlayerData(this ulong playerId) {
+    return PlayerService.TryGetById(playerId, out PlayerData playerData) ? playerData : null;
+  }
+
+  public static PlayerData GetPlayerData(this Entity entity) {
+    if (entity.Has<User>()) {
+      return PlayerService.TryGetById(entity.Read<User>().PlatformId, out PlayerData playerData) ? playerData : null;
+    }
+
+    if (entity.Has<PlayerCharacter>()) {
+      var playerCharacter = entity.Read<PlayerCharacter>();
+      var user = playerCharacter.UserEntity.Read<User>();
+      return PlayerService.TryGetById(user.PlatformId, out PlayerData playerData) ? playerData : null;
+    }
+
+    return null;
+  }
+
+  public static PlayerData GetPlayerData(this NetworkId networkId) {
+    return PlayerService.TryGetByNetworkId(networkId, out PlayerData playerData) ? playerData : null;
+  }
+}
