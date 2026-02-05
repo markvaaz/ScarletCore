ccf9200fff89fb77d2420a77b5864cb40f940d0f
markvaaz <marcosrodolfo32@gmail.com>
2026-02-05 09:19:53 -0300

    fix: remove unsafe modifier from PerformanceRecorderSystemPatch class

--- Patches/OnUpdatePatch.cs
+++ Patches/OnUpdatePatch.cs
@@ -6,7 +6,7 @@ using Unity.Entities;
 namespace ScarletCore.Patches;
 
 [HarmonyPatch]
-internal unsafe class PerformanceRecorderSystemPatch {
+internal class PerformanceRecorderSystemPatch {
 
   [HarmonyPatch(typeof(PerformanceRecorderSystem), nameof(PerformanceRecorderSystem.OnUpdate))]
   [HarmonyPrefix]
